e81ec414220c1ea96fcdbbf08975654ac0d03ba2
Bradley <brad@Bradleys-MacBook-Pro.local>
2018-01-02 17:16:32 -1000

    allPassed

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,12 @@
 'use strict';
 module.exports = function() {
-  
+    let randomNum = Math.floor(Math.random()*1000000);
+
+    return function(){
+        let num1 = randomNum;
+        return num1;
+    }
+
+
+
 };
